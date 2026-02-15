152ddcaebb424f3733e4839136b70ee83bb84f5b
Aviad Shiber <aviad.s@taboola.com>
2026-02-15 22:37:10 +0200

    Fix formula: use venv with pip install for dependency resolution

--- Formula/langfuse-cli.rb
+++ Formula/langfuse-cli.rb
@@ -1,6 +1,4 @@
 class LangfuseCli < Formula
-  include Language::Python::Virtualenv
-
   desc "CLI tool for Langfuse LLM observability platform"
   homepage "https://github.com/aviadshiber/langfuse-cli"
   url "https://files.pythonhosted.org/packages/source/l/langfuse-cli/langfuse_cli-0.1.0.tar.gz"
@@ -10,9 +8,12 @@ class LangfuseCli < Formula
   depends_on "python@3.12"
 
   def install
-    venv = virtualenv_create(libexec, "python3.12")
-    venv.pip_install buildpath
-    bin.install_symlink Dir[libexec/"bin/lf"]
+    python3 = "python3.12"
+    venv = libexec/"venv"
+    system python3, "-m", "venv", venv
+    system venv/"bin/pip", "install", "--upgrade", "pip"
+    system venv/"bin/pip", "install", buildpath
+    bin.install_symlink Dir[venv/"bin/lf"]
   end
 
   test do
