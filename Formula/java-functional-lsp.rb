d5bb882e989fc213c90cc4fa8d4738b178330266
Aviad Shiber <aviad.s@taboola.com>
2026-03-21 21:26:04 +0200

    Update java-functional-lsp to 0.4.0 (fix PyPI URL)

--- Formula/java-functional-lsp.rb
+++ Formula/java-functional-lsp.rb
@@ -1,7 +1,7 @@
 class JavaFunctionalLsp < Formula
   desc "Java LSP server enforcing functional programming best practices"
   homepage "https://github.com/aviadshiber/java-functional-lsp"
-  url "https://files.pythonhosted.org/packages/source/j/java-functional-lsp/java_functional_lsp-0.4.0.tar.gz"
+  url "https://files.pythonhosted.org/packages/2e/b6/7898b162fdceb558e68f7a78fd290e36fa64495a90cf651825c555f66530/java_functional_lsp-0.4.0.tar.gz"
   sha256 "09dd12b2d80d3591f967d80cffce020f20d72550b4781131455001ccf579e6ea"
   license "MIT"
 
@@ -21,4 +21,3 @@ class JavaFunctionalLsp < Formula
     assert_match "java-functional-lsp", shell_output("#{bin}/java-functional-lsp --help 2>&1", 1)
   end
 end
-
